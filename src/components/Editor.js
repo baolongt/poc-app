c1a5df899a85ff36b2b156e01d8847f9812235b7
Tran Long <tranlong@Trans-MBP-2.lan>
2023-05-20 19:19:59 +0700

    update to enable style while import word

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,5 +1,4 @@
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-
 import { Essentials } from '@ckeditor/ckeditor5-essentials';
 import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
 import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
@@ -13,6 +12,7 @@ import HeadingPlugin from '@ckeditor/ckeditor5-heading/src/heading';
 import LinkPlugin from '@ckeditor/ckeditor5-link/src/link';
 import ParagraphPlugin from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import { DocumentList, DocumentListProperties } from '@ckeditor/ckeditor5-list';
+import { GeneralHtmlSupport } from '@ckeditor/ckeditor5-html-support';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -33,6 +33,7 @@ ClassicEditor.builtinPlugins = [
   ParagraphPlugin,
   DocumentList,
   DocumentListProperties,
+  GeneralHtmlSupport,
 ];
 
 // Editor configuration.
@@ -47,13 +48,6 @@ ClassicEditor.defaultConfig = {
       'bulletedList',
       'numberedList',
       '|',
-      'outdent',
-      'indent',
-      '|',
-      'imageUpload',
-      'blockQuote',
-      'insertTable',
-      'mediaEmbed',
       'undo',
       'redo',
       'importWord',
@@ -62,6 +56,17 @@ ClassicEditor.defaultConfig = {
   importWord: {
     tokenUrl:
       'https://97308.cke-cs.com/token/dev/0e6d17ce0b94ea7fa69a9da4f722d0cfabdae8cc0bc13768ee9f97379800?limit=10',
+    defaultStyles: true,
+  },
+  htmlSupport: {
+    allow: [
+      {
+        name: /.*/,
+        attributes: true,
+        classes: true,
+        styles: true,
+      },
+    ],
   },
   cloudServices: {
     tokenUrl:
